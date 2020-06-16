2298f465ea4f6b9d1bcb133e0857ddb92a9d3051
Devin Clarke <devinlclarke@gmail.com>
2020-06-16 19:25:18 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def say_hello (kent Beck = Ruby Programmer)
+def say_hello (kent beck = Ruby Programmer)
   puts "Hello #{name}."
 end
 
-def say_hello (kent Beck)
+def say_hello (kent beck)
   puts "Hello #{name}."
 end
